bd177d8b44459a1fa5e2a657365df26ca095ba31
Alexandre Dulaunoy <a@foo.be>
2011-04-01 14:01:13 +0200

    Caching and checking added

--- bin/getranking.pl
+++ bin/getranking.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/perl
+
 $| = 1;
 use Redis;
 use Date::Calc qw(Add_Delta_Days Day_of_Week);
@@ -25,21 +27,69 @@ my @Source = (
 
 my $r = Redis->new( server => '149.13.33.68:6379' );
 $r->select("6");
-$asn = $ARGV[0];
+my $rc = Redis->new( server => '127.0.0.1:6379' );
+$rc->select("2");
+my $asn = "";
 
-my $total = 1;
-foreach my $lsource (@Source) {
+$asn = <STDIN>;
+chomp($asn);
+if ( !checkASN($asn) ) {
+    print "ASN format incorrect";
+    ByeBye();
+}
+my ( $total, $visibility ) = fetchASN($asn);
+$value = $asn . "," . $total . "," . $visibility;
 
-    $key = $asn . "|" . $yesterday . "|" . $lsource . "|rankv4";
+print $value. "\n";
+cacheValue($value);
+ByeBye();
 
-    if ( $r->exists($key) ) {
-        my $rankingvalue = $r->get($key);
-        my $decrankingvalue = sprintf( "%.20f", $rankingvalue );
+sub ByeBye {
+    $r->quit();
+    $rc->quit();
+    exit();
+}
 
-        #print $key."=". $decrankingvalue ."\n";
-        $total = $total + $decrankingvalue;
+sub cacheValue {
+    my $value = shift;
+    $key = "rank|" . $asn . "|" . $yesterday . "|c4";
+    $rc->set( $key => $value );
+}
+
+sub checkASN {
+    my $asn = shift;
+    if ( $asn =~
+/^(429496729[0-6]|42949672[0-8]\d|4294967[01]\d{2}|429496[0-6]\d{3}|42949[0-5]\d{4}|4294[0-8]\d{5}|429[0-3]\d{6}|42[0-8]\d{7}|4[01]\d{8}|[1-3]\d{9}|[1-9]\d{8}|[1-9]\d{7}|[1-9]\d{6}|[1-9]\d{5}|[1-9]\d{4}|[1-9]\d{3}|[1-9]\d{2}|[1-9]\d|\d)$/
+      )
+    {
+        return 1;
     }
+    return undef;
 
 }
-print $total. "\n";
-$r->quit();
+
+sub fetchASN {
+
+    my $asn         = shift;
+    my $total       = 1;
+    my $sourcetotal = 1;
+    my $sourceview  = 0;
+    foreach my $lsource (@Source) {
+
+        $key = $asn . "|" . $yesterday . "|" . $lsource . "|rankv4";
+
+        if ( $r->exists($key) ) {
+            my $rankingvalue = $r->get($key);
+            my $decrankingvalue = sprintf( "%.20f", $rankingvalue );
+
+            #print $key."=". $decrankingvalue ."\n";
+            $total      = $total + $decrankingvalue;
+            $sourceview = $sourceview + 1;
+        }
+        $sourcetotal = $sourcetotal + 1;
+
+    }
+    my $visibility = $sourceview . "/" . $sourcetotal;
+    return ( $total, $visibility );
+
+}
