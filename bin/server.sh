ca8823ff36cd82e3c98c7cd158f83b9f4f39e07b
Alexandre Dulaunoy <a@foo.be>
2011-04-01 22:41:55 +0200

    suppress echo

--- bin/server.sh
+++ bin/server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo socat TCP6-LISTEN:43,fork,tcpwrap=script,ipv6only=0  EXEC:/home/adulau/git/bgpranking-api/bin/getranking.pl,su-d=adulau,pty,stderr
+sudo socat TCP6-LISTEN:43,fork,tcpwrap=script,ipv6only=0  EXEC:/home/adulau/git/bgpranking-api/bin/getranking.pl,su-d=adulau,pty,stderr,echo=0
 
