57a9832f6e7a751adee16b6f758a95e6ad05ab39
Alexandre Dulaunoy <a@foo.be>
2011-04-25 22:04:22 +0200

    Basic explanation added

--- bin/getranking.pl
+++ bin/getranking.pl
@@ -1,6 +1,31 @@
 #!/usr/bin/perl
+#
+# BGP Ranking API - simple API (whois-like) to get ASN ranking value
+#
+# To run your own, you'll need the following:
+#
+# - A BGP Ranking installation (https://github.com/Rafiot/bgp-ranking)
+# - An additional Redis server for caching computed values
+#
+# Copyright (C) 2011 Alexandre Dulaunoy
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 $| = 1;
+
+my $redisbgpranking = "1.2.3.4:6379";
+my $rediscaching = "127.0.0.0:6379";
 
 use strict;
 use warnings;
@@ -29,9 +54,9 @@ my @Source = (
     "AmadaIpBlockList",       "BlocklistDe"
 );
 
-my $r = Redis->new( server => '149.13.33.68:6379' );
+my $r = Redis->new( server => $redisbgpranking );
 $r->select("6");
-my $rc = Redis->new( server => '127.0.0.1:6379' );
+my $rc = Redis->new( server => $rediscaching );
 $rc->select("2");
 my $asn = "";
 
@@ -45,8 +70,8 @@ if ( !checkASN($asn) ) {
 my ( $total, $visibility ) = fetchASN($asn);
 my $value = $asn . "," . $total . "," . $visibility;
 
-print $value. "\n";
 cacheValue($value);
+print $value. "\n";
 ByeBye();
 
 sub ByeBye {
