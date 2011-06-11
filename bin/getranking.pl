1083c4c483345056a5c8e2d6fdc091f30c2930b9
Alexandre Dulaunoy <a@foo.be>
2011-06-11 11:58:34 +0200

    Minimal syslog logging added

--- bin/getranking.pl
+++ bin/getranking.pl
@@ -28,6 +28,8 @@ use Redis;
 use Date::Calc qw(Add_Delta_Days Day_of_Week);
 use Date::Calc qw(Today Add_Delta_Days);
 use Number::FormatEng qw(:all);
+use Env qw(SOCAT_PEERADDR);
+use Sys::Syslog qw( :DEFAULT setlogsock);
 
 my @date = Add_Delta_Days( Today(), -1 );
 
@@ -66,7 +68,13 @@ my ( $total, $visibility, $best, $score ) = fetchASN($asn);
 my $value =
   $asn . "," . $total . "," . $visibility . "," . $best . "," . $score;
 
-print "# ASN,Rank,Matched BL,Best Ranking,Current Position\n";
+#print STDERR $SOCAT_PEERADDR . "-->".$value;
+setlogsock('unix');
+openlog($0,'','user');
+syslog('info',$SOCAT_PEERADDR . "-->".$value);
+closelog;
+
+print "# ASN,Rank,Matched BL,Highest Malicious Ranking,Current Position\n";
 print $value. "\n";
 cacheValue($value);
 ByeBye();
